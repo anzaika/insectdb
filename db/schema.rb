272c684c8312ee379e2159e05fec32d086a7016e
Andrei Zaika <anzaika@gmail.com>
2014-03-06 13:50:58 +0400

    finish refactoring seed code

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,81 @@ ActiveRecord::Schema.define(version: 20140303162714) do
   enable_extension "plpgsql"
   enable_extension "hstore"
 
+  create_table "divs", force: true do |t|
+    t.integer "chromosome"
+    t.integer "position"
+    t.text    "alleles"
+  end
+
+  add_index "divs", ["chromosome", "position"], name: "divs__chr_pos", using: :btree
+
+  create_table "genes", id: false, force: true do |t|
+    t.integer "id",         null: false
+    t.string  "flybase_id"
+  end
+
+  create_table "genes_mrnas", id: false, force: true do |t|
+    t.integer "gene_id"
+    t.integer "mrna_id"
+  end
+
+  add_index "genes_mrnas", ["gene_id"], name: "genes_mrnas__gene_id", using: :btree
+  add_index "genes_mrnas", ["mrna_id"], name: "genes_mrnas__mrna_id", using: :btree
+
+  create_table "mrnas", id: false, force: true do |t|
+    t.integer "id",         null: false
+    t.integer "chromosome"
+    t.string  "strand"
+    t.integer "start"
+    t.integer "stop"
+    t.text    "_ref_seq"
+  end
+
+  create_table "mrnas_segments", id: false, force: true do |t|
+    t.integer "mrna_id"
+    t.integer "segment_id"
+  end
+
+  add_index "mrnas_segments", ["mrna_id"], name: "mrnas_segments__mrna_id", using: :btree
+  add_index "mrnas_segments", ["segment_id"], name: "mrnas_segments__segment_id", using: :btree
+
+  create_table "reference", force: true do |t|
+    t.integer "chromosome"
+    t.integer "position"
+    t.string  "dmel"
+    t.string  "dsim"
+    t.string  "dyak"
+  end
+
+  add_index "reference", ["chromosome", "position"], name: "ref__chr_pos", using: :btree
+
   create_table "results", force: true do |t|
     t.hstore   "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "segments", id: false, force: true do |t|
+    t.integer "id",         null: false
+    t.integer "chromosome"
+    t.integer "start"
+    t.integer "stop"
+    t.integer "length"
+    t.string  "type"
+    t.text    "_ref_seq"
+  end
+
+  add_index "segments", ["chromosome", "start", "stop"], name: "segments__chr_start_stop", using: :btree
+  add_index "segments", ["type"], name: "segments__type", using: :btree
+
+  create_table "snps", force: true do |t|
+    t.integer "chromosome"
+    t.integer "position"
+    t.integer "sig_count"
+    t.integer "aaf"
+    t.text    "alleles"
+  end
+
+  add_index "snps", ["chromosome", "position", "sig_count"], name: "snps__chr_sigcount_pos", using: :btree
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require_relative 'seeds/seq_seed'
+require_relative 'seeds/annotation_seed'
+
+SeqSeed.for_each_chromosome
+AnnotationSeed.new.start
