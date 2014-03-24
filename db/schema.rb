a3e4ba42e0e0173d0bfafd881e27c066498a4705
Andrei Zaika <anzaika@gmail.com>
2014-03-24 17:34:52 +0400

    remove reference table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315071427) do
+ActiveRecord::Schema.define(version: 20140310062859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "divs", force: true do |t|
     t.integer "chromosome"
@@ -38,14 +37,18 @@ ActiveRecord::Schema.define(version: 20140315071427) do
   add_index "genes_mrnas", ["gene_id"], name: "genes_mrnas__gene_id", using: :btree
   add_index "genes_mrnas", ["mrna_id"], name: "genes_mrnas__mrna_id", using: :btree
 
-  create_table "mrnas", id: false, force: true do |t|
-    t.integer "id",         null: false
+  create_table "mrnas", force: true do |t|
     t.integer "chromosome"
     t.string  "strand"
     t.integer "start"
     t.integer "stop"
     t.text    "_ref_seq"
+    t.boolean "good_quality"
+    t.string  "bad_quality_reason"
   end
+
+  add_index "mrnas", ["good_quality"], name: "mrna__good_quality", using: :btree
+  add_index "mrnas", ["id"], name: "mrna__id", using: :btree
 
   create_table "mrnas_segments", id: false, force: true do |t|
     t.integer "mrna_id"
@@ -55,34 +58,19 @@ ActiveRecord::Schema.define(version: 20140315071427) do
   add_index "mrnas_segments", ["mrna_id"], name: "mrnas_segments__mrna_id", using: :btree
   add_index "mrnas_segments", ["segment_id"], name: "mrnas_segments__segment_id", using: :btree
 
-  create_table "reference", force: true do |t|
-    t.integer "chromosome"
-    t.integer "position"
-    t.string  "dmel"
-    t.string  "dsim"
-    t.string  "dyak"
-  end
-
-  add_index "reference", ["chromosome", "position"], name: "ref__chr_pos", using: :btree
-
-  create_table "results", force: true do |t|
-    t.hstore   "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "segments", id: false, force: true do |t|
-    t.integer "id",         null: false
+  create_table "segments", force: true do |t|
     t.integer "chromosome"
     t.integer "start"
     t.integer "stop"
     t.integer "length"
     t.string  "type"
     t.text    "_ref_seq"
+    t.string  "inclusion_pattern"
+    t.string  "segment_gain"
   end
 
-  add_index "segments", ["chromosome", "start", "stop"], name: "segments__chr_start_stop", using: :btree
-  add_index "segments", ["type"], name: "segments__type", using: :btree
+  add_index "segments", ["id"], name: "segment__id", using: :btree
+  add_index "segments", ["type", "chromosome"], name: "segments__type_chromosome", using: :btree
 
   create_table "seqs", force: true do |t|
     t.integer "chromosome"
@@ -97,10 +85,10 @@ ActiveRecord::Schema.define(version: 20140315071427) do
     t.integer "chromosome"
     t.integer "position"
     t.integer "sig_count"
-    t.text    "alleles"
     t.float   "aaf"
+    t.text    "alleles"
   end
 
-  add_index "snps", ["chromosome", "position", "sig_count"], name: "snps__chr_sigcount_pos", using: :btree
+  add_index "snps", ["chromosome", "position", "sig_count", "aaf"], name: "snps__chr_sigcount_pos_aaf", using: :btree
 
 end
